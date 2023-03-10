edcfdda3f32ebc11ede3a1796b123c3204c0627e
Indy Nathaphon <nathaphon@primo.mobi>
2023-03-10 15:32:57 +0700

    add promotion code model

---
+++ src/models/promotion-code/promotion-code.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const promotionCode = new Schema(
+  {
+    promotionCodeId: { type: String, required: true, unique: true },
+    promotionCodeName: { type: String, required: true, unique: true },
+    companyId: { type: String, required: true, unique: true },
+    discount: { type: Number, required: true },
+    description: { type: String, required: false },
+    quantity: { type: Number, required: true },
+    used: { type: Number, required: true },
+    start: { type: Date, required: true },
+    expires: { type: Date, required: true },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+const PromotionCode = mongoose.model("promotion-code", promotionCode);
+module.exports = { PromotionCode };
